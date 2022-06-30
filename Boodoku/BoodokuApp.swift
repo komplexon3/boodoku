25f124627b959adfa362d8bc71e64bb08d77e1ef
Marc Widmer <mwidmer@protonmail.com>
2022-06-30 10:02:20 -0400

    Initial Commit

---
+++ Boodoku/BoodokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoodokuApp.swift
+//  Boodoku
+//
+//  Created by Marc Widmer on 30.06.22.
+//
+
+import SwiftUI
+
+@main
+struct BoodokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
