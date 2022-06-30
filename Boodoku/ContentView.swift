4e0d538d60cc283fc64eac0913b0e4fffe78520b
Marc Widmer <mwidmer@protonmail.com>
2022-06-30 10:50:12 -0400

    initial setup

--- Boodoku/ContentView.swift
+++ Boodoku/ContentView.swift
@@ -7,10 +7,35 @@
 
 import SwiftUI
 
+struct NumberButton: View {
+    let number: Int
+    var body: some View {
+        Button(action: {}) {
+            Text("\(number)").font(.title)
+        }
+    }
+}
+
+struct Grid: View {
+    var body: some View {
+        Image("grid")
+            .resizable()
+            .padding(.all, 0.0/*@END_MENU_TOKEN@*/)
+            .frame(width: /*@START_MENU_TOKEN@*/390.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/390.0/*@END_MENU_TOKEN@*/)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Grid()
+            HStack {
+                ForEach((1...9), id: \.self) {num in
+                    NumberButton(number: num)
+                        .frame(maxWidth: .infinity)
+                }
+            }.padding(30)
+        }
     }
 }
 
